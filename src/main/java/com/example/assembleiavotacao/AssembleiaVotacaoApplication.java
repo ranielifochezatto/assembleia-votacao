0526814bc74266b489dd57bc566cab7fcb56f9fd
Ranieli Fochezatto <ranieli.fochezatto@Ranielis-MacBook-Air.local>
2021-05-30 19:51:37 -0300

    feat(projeto) estrutura inicial

---
+++ src/main/java/com/example/assembleiavotacao/AssembleiaVotacaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.assembleiavotacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssembleiaVotacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssembleiaVotacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/assembleiavotacao/AssembleiaVotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assembleiavotacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssembleiaVotacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
